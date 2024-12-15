135458abe831362f4fa77c99142928003d7096ac
feyza yarcı <yarcifeyza@gmail.com>
2024-12-15 20:49:15 +0300

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,27 @@
+const images = [
+    {
+        url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+        alt: "White and Black Long Fur Cat",
+    },
+    {
+        url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+        alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    },
+    {
+        url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+        alt: "Group of Horses Running",
+    },
+];
+const gallery = document.querySelector(".gallery");   //class ı gallery olanı yakaladım ve gallery sabit değişkenine attım.
+
+images.forEach(function (image) {
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = image.url;
+    img.alt = image.alt;
+
+    li.appendChild(img);
+    gallery.appendChild(li);
+});
+
 
